5d041fb80d35ffb8ff12629a8ae85e15b876f0b3
Nadeem Rana <nadeem.rana0257@gmail.com>
2025-05-15 05:18:50 +0500

    Final modification

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ if (allowedOrigins) {
 }
 
 app.use(cors({
-  origin: "https://localhost:3000",
+  origin: "http://localhost:3000",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
